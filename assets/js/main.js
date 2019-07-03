1476bbfdc73be1f7be84975ee91949f279a849e1
Ty Walls <tygerbytes@tygertec.com>
2019-07-03 11:16:57 -0700

    Make header slide out of the way on scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,61 @@
 
 (function() {
+
+  function throttle(func, params) {
+    if (func.throttled) {
+      return;
+    }
+    func.throttled = true;
+    func(params);
+    window.setTimeout(() => func.throttled = false, 300);
+  }
+
+  var header = {
+    me: document.getElementById('page-header'),
+    links: document.getElementById('header-links'),
+    logo: document.getElementById('header-logo'),
+    lastY: 999999,
+
+    toggleVisibility: function() {
+      header.links.classList.toggle('visible');
+      header.logo.classList.toggle('visible-override');
+      this.me.classList.toggle('hamburger-mode');
+    },
+
+    toggleSticky: function(scrollY) {
+      var atTopOfWindow = scrollY < 100;
+      var scrollingUp = scrollY < this.lastY;
+      var hamburgerMenuOpen = hamburger.isToggled;
+
+      var isSticky = (atTopOfWindow||  hamburgerMenuOpen || scrollingUp);
+      this.me.classList.toggle('sticky', isSticky);
+
+      this.lastY = scrollY;
+    }
+  };
+
   var hamburger = {
     toggleButton: document.getElementById('hamburger-toggle'),
-
-    headerLinks: document.getElementById('header-links'),
-    siteMenu: document.querySelector('.site-menu'),
-    headerLogo: document.getElementById('header-logo'),
+    menu: document.querySelector('.site-menu'),
+    isToggled: false,
 
     toggleMenu:  function(e) {
       e.preventDefault();
       this.toggleButton.classList.toggle('expanded');
-      this.headerLinks.classList.toggle('visible');
-      this.siteMenu.classList.toggle('visible');
-      this.headerLogo.classList.toggle('visible-override');
+      this.isToggled = (this.toggleButton.classList.contains('expanded'));
+      this.menu.classList.toggle('visible');
+      header.toggleVisibility();
     }
   };
 
   hamburger.toggleButton.addEventListener('click', function(e) {
     hamburger.toggleMenu(e);
   });
+
+  function scrollHandler(e) {
+    let scrollY = window.scrollY;
+    header.toggleSticky(scrollY);
+  }
+  window.addEventListener('scroll', (e) => throttle(scrollHandler, e));
+
 })();
